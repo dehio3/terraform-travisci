dc0ce0125714573918325feb21d5569412a45155
dehio3 <dehitomo0318@gmail.com>
2019-09-24 17:02:04 +0900

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 module "ec2_test" {
   source                 = "terraform-aws-modules/ec2-instance/aws"
-  version                = "1.21.0"
+  version                = "~> 2.0"
   name                   = "ec2-test"
   instance_count         = 1
   ami                    = "ami-0b8d0d6ac70e5750c" #amzn2-ami-hvm-2.0.20181114-x86_64-ebs
